68c7b15961a64441eb5b9d253c20011acd5e2fef
Antoine de Saint Germain <adsg96@hku.hk>
2025-04-04 17:33:52 +0800

    first bits of lean

--- RootSystem/Basic.lean
+++ RootSystem/Basic.lean
@@ -1,2 +1,21 @@
 import Mathlib.LinearAlgebra.RootSystem.Defs
-#check RootPairing.pairing
+import Mathlib.LinearAlgebra.RootSystem.Reduced
+
+
+variable {ι R M N : Type*} [CommRing R] [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
+  (P : RootPairing ι R M N) (S : Type*) {i j : ι}
+
+namespace RootPairing
+lemma name? (hP : IsReduced P) (i j : ι) (h : ¬ LinearIndependent R ![P.coroot i, P.coroot j]) :
+    P.coroot i = P.coroot j ∨ P.coroot i = - P.coroot j := sorry
+
+lemma flip_still_reduced (hP : IsReduced P) : IsReduced (P.flip) := by
+    intro i j h
+    have key : P.flip.root = P.coroot := rfl
+    rw [key]
+    exact name? P hP i j h
+
+#print RootPairing.flip_still_reduced
+
+
+end RootPairing

31d4fd984c8bf88d03601e1cdc90d71bad83299d
Antoine de Saint Germain <adsg96@hku.hk>
2025-04-10 17:12:30 +0800

    pairing is W-invariant lean

--- RootSystem.lean
+++ RootSystem.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `RootSystem` library.
 -- Import modules here that should be built as part of the library.
 import «RootSystem».Basic
+import «RootSystem».LocalDefs
---
+++ RootSystem/LocalDefs.lean
@@ -0,0 +1,11 @@
+import Mathlib.LinearAlgebra.RootSystem.Defs
+
+variable {ι R M N : Type*} [CommRing R] [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
+  (P : RootPairing ι R M N) (S : Type*) {i j k : ι}
+
+namespace RootPairing
+
+lemma pairing_W_invariant : P.pairing (P.reflection_perm k i) (P.reflection_perm k j) = P.pairing i j := by sorry
+
+
+end RootPairing

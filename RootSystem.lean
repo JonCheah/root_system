2505b67a29489858f1e1c4ce9aaf196a92777793
Antoine de Saint Germain <adsg96@hku.hk>
2025-04-23 23:23:57 +0800

    restructure blueprint
    - added proofs of lemmas in basicDefs
    - re-organised blueprint
    - added blueprint tags \lean and \leanok

--- RootSystem.lean
+++ RootSystem.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `RootSystem` library.
 -- Import modules here that should be built as part of the library.
-import «RootSystem».Basic
-import «RootSystem».LocalDefs
+
+import «RootSystem».rootPairing.basicDefs
--- RootSystem/rootPairing/basicDefs.lean
+++ RootSystem/rootPairing/basicDefs.lean
@@ -4,9 +4,18 @@ import Mathlib.LinearAlgebra.RootSystem.WeylGroup
 variable {ι R M N : Type*} [CommRing R] [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
   (P : RootPairing ι R M N) (S : Type*) {i j k : ι}
 
-#check IsSMulRegular
-#check RootPairing.reflection_perm_eq_reflection_perm_iff_of_isSMulRegular
 namespace RootPairing
+
+-- Define an instance of a perfect pairing?
+
+
+lemma reflection_perm_unique {Q : RootPairing ι R M N} {h1 : Q.toPerfectPairing = P.toPerfectPairing} {h2 : Q.root = P.root} {h3 : Q.coroot = P.coroot} :
+    Q.reflection_perm = P.reflection_perm := by
+  ext i j
+  apply P.root.injective
+  nth_rewrite 1 [← h2]
+  rw [← Q.reflection_perm_root, ← P.reflection_perm_root, h1, h2, h3]
+
 
 -- This is a terrible proof
 lemma pairing_si_invariant : P.pairing (P.reflection_perm k i) (P.reflection_perm k j) = P.pairing i j := by

3b464e7fe9f47f65ee566f60af988b4c3449ff1f
Antoine de Saint Germain <adsg96@hku.hk>
2025-04-23 16:40:32 +0800

    Update Basic.lean

--- RootSystem/Basic.lean
+++ RootSystem/Basic.lean
@@ -7,17 +7,6 @@ variable {ι R M N : Type*} [CommRing R] [AddCommGroup M] [Module R M] [AddCommG
 
 
 namespace RootPairing
-lemma name? (hP : IsReduced P) (i j : ι) (h : ¬ LinearIndependent R ![P.coroot i, P.coroot j]) :
-    P.coroot i = P.coroot j ∨ P.coroot i = - P.coroot j := sorry
-
-lemma flip_still_reduced (hP : IsReduced P) : IsReduced (P.flip) := by
-    intro i j h
-    have key : P.flip.root = P.coroot := rfl
-    rw [key]
-    exact name? P hP i j h
-
-#print RootPairing.flip_still_reduced
-
 lemma reflection_perm_unique {Q : RootPairing ι R M N} {h1 : Q.toPerfectPairing = P.toPerfectPairing} {h2 : Q.root = P.root} {h3 : Q.coroot = P.coroot} :
     Q.reflection_perm = P.reflection_perm := by
   ext i j

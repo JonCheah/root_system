e0cb764249d1e396747cf5a19d93c6da56373366
Antoine de Saint Germain <adsg96@hku.hk>
2025-04-23 15:58:17 +0800

    proof lemma 6

--- RootSystem/Basic.lean
+++ RootSystem/Basic.lean
@@ -18,5 +18,12 @@ lemma flip_still_reduced (hP : IsReduced P) : IsReduced (P.flip) := by
 
 #print RootPairing.flip_still_reduced
 
+lemma reflection_perm_unique {Q : RootPairing ι R M N} {h1 : Q.toPerfectPairing = P.toPerfectPairing} {h2 : Q.root = P.root} {h3 : Q.coroot = P.coroot} :
+    Q.reflection_perm = P.reflection_perm := by
+  ext i j
+  apply P.root.injective
+  nth_rewrite 1 [← h2]
+  rw [← Q.reflection_perm_root, ← P.reflection_perm_root, h1, h2, h3]
+
 
 end RootPairing
